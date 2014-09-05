2b399a830757a050a3862bb099f1345a8e885921
unknown <cakacimi@697-334-Bree.sf.k12.internal>
2014-09-05 10:15:37 -0700

    Lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,54 @@
+int startX=200;
+int startY=10;
+int endX=150;
+int endY=0;
+
 void setup()
 {
-  size(300,300);
+  size(500,500);
+  background(0);
+  strokeWeight(50);
 }
 void draw()
 {
+  background(0);
+  house();
 
+  stroke(255,255,102);
+  while(endY<500)
+  {
+    endX = startX+(int)(random(-9,9));
+    endY = startY+(int)(random(0,9));
+    line(startX,startY,endX,endY);
+    startX = endX;
+    startY = endY;
+  }
 }
-void mousePressed()
+void keyPressed()
 {
-
+  startX=150; 
+  startY=60;
+  endX=150;
+  endY=0;
+  
 }
 
+void house()
+{
+  stroke(0);
+  strokeWeight(2);
+  //frame//
+  fill(255,200,255);
+  rect(205,375,100,100);
+  //roof//
+  fill(255,0,0);
+  triangle(205,375,305,375,250,320);
+  //door//
+  fill(102,51,0);
+  rect(245,425,20,50);
+  //windows//
+   fill(225);
+   rect(215,390,20,20);
+    rect(275,390,20,20);
+    
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,60 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX=200;
+int startY=10;
+int endX=150;
+int endY=0;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(500,500);
+  background(0);
+  strokeWeight(50);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+  background(0);
+  house();
+
+  stroke(255,255,102);
+  while(endY<500)
+  {
+    endX = startX+(int)(random(-9,9));
+    endY = startY+(int)(random(0,9));
+    line(startX,startY,endX,endY);
+    startX = endX;
+    startY = endY;
+  }
 }
-public void mousePressed()
+public void keyPressed()
 {
-  redraw();
+  startX=150; 
+  startY=60;
+  endX=150;
+  endY=0;
+  
 }
 
+public void house()
+{
+  stroke(0);
+  strokeWeight(2);
+  //frame//
+  fill(255,200,255);
+  rect(205,375,100,100);
+  //roof//
+  fill(255,0,0);
+  triangle(205,375,305,375,250,320);
+  //door//
+  fill(102,51,0);
+  rect(245,425,20,50);
+  //windows//
+   fill(225);
+   rect(215,390,20,20);
+    rect(275,390,20,20);
+    
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
